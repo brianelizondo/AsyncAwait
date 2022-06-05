5c6560614133f2b8671e6ceaeeda9e9e170b6a87
Brian Elizondo <brian_elizondo@msn.com>
2022-06-05 09:51:42 -0400

    Part 1: Number Facts => 1. Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number. (Make sure you get back JSON by including the json query key, specific to this API

--- static/app.js
+++ static/app.js
@@ -7,13 +7,18 @@ $(document).ready(function(){
     // Part 1: Number Facts
     // 1. Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number
     let div_show_1_1 = "part_1_1";
-    let numberFactsURL = "http://numbersapi.com/random";
-    let numberFactsPromise = axios.get(numberFactsURL);
     
-    numberFactsPromise
-        .then(resp => showResponse(div_show_1_1, resp.data))
-        .catch(err => showResponse(div_show_1_1, err));
-
+    async function getNumberFacts(){
+        try{
+            let numberFactsURL = "http://numbersapi.com/random";
+            let response = await axios.get(numberFactsURL);
+            showResponse(div_show_1_1, response.data);
+        } catch (err){
+            showResponse(div_show_1_1, err);
+        }
+    }
+    getNumberFacts();
+    
 
     // 2. Figure out how to get data on multiple numbers in a single request.
     // Make that request and when you get the data back, put all of the number facts on the page
